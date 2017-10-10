a6f102fd2eccb60f7e2ce138088a6a76b618594c
evatlsong <evatlsong@gmail.com>
2017-10-10 10:11:18 +0800

    add 3.3.9

---
+++ 3.3.9/alpine/docker-entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+if [ "$1" = 's' ] || [ "$1" = 'server' ]; then
+    set -- hexo s -p 80
+fi
+
+if [ "$1" = 'd' ] || [ "$1" = 'deploy' ]; then
+    set -- hexo cl && hexo d -g
+fi
+
+exec "$@"
